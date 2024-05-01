af219b2e4749ea93f5e114545ee59b12d43a3eef
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-05-01 15:38:14 -0300

    feat: add colors to worker scripts

--- scripts/workers.sh
+++ scripts/workers.sh
@@ -3,12 +3,12 @@
 set -e
 
 python3 -u ./src/video_downloader.py 2>&1 | while read -r line; do
-  echo "[downloader] $line"
+  printf "\e[36m[downloader]\e[0m %s\n" "$line"
 done &
 
 for resolution in "${@}"; do
   python3 -u ./src/video_converter.py "$resolution" 2>&1 | while read -r line; do
-    echo "[converter $resolution] $line"
+    printf "\e[33m[converter %s]\e[0m %s\n" "$resolution" "$line"
   done &
 done
 
